48d24db7654c6ce79cf7b68621b514f617cf6fc4
Sean McGinnis <seankmcginnis@gmail.com>
2014-06-23 16:20:34 -0400

    TimePrimitives and Initial GxTour Insert
    Added time stamps to the points and created logic to support initial
    GxTour and GxFlyTo tags

--- Python/GPX_to_KML.py
+++ Python/GPX_to_KML.py
@@ -1,29 +1,38 @@
 import gpxpy
 import gpxpy.gpx
 import simplekml
+## Variables to be transferred to inputs in UI script
+tour_name = 'Test'
+gpx = 'SampleData/140606.gpx'
+flyto_duration = 2
 
+# Create the output KML File
 kml = simplekml.Kml()
+# Create the tour
+tour = kml.newgxtour(name=tour_name)
+# Create a playlist in the tour
+playlist = tour.newgxplaylist()
 
-gpx_file = open('SampleData/140606.gpx')
+#get the raw GPX information
+gpx_file = open(gpx)
 gpx = gpxpy.parse(gpx_file)
 
+# Let's make sure we see whenever someone hit the 'Red Button' and iterate through the waypoints
 for waypoint in gpx.waypoints:
-	#print 'waypoint {0} -> ({1},{2})'.format(waypoint.name, , )
-	#kml.newpoint(name="Red Button", coords=[(waypoint.latitude, waypoint.longitude)])
 	kml.newpoint(name="Red Button", coords=[(waypoint.longitude, waypoint.latitude)])
 
-track_counter = 0
 for track in gpx.tracks:
-	track_counter = track_counter + 1
-	print 'Track #%s' % str(track_counter)
 	for segment in track.segments:
 		points = []
 		for point in segment.points:
 			coord = (point.longitude, point.latitude)
+			#print(point.time)
 			pnt = kml.newpoint(coords=[coord])
+			pnt.timestamp.when = point.time
 			pnt.style.iconstyle.icon.href= 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
 			points.append(coord)
-			#print 'Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation)
+			flyto = playlist.newgxflyto(gxduration=flyto_duration)
+			
 
 lin = kml.newlinestring(name="Test", description="Test", coords=points)
 kml.save("test.kml")
