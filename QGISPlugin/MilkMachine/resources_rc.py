d85bc0b21f89bd84d14787481634e756e21a9331
EdFarrell <eddief77@yahoo.com>
2014-11-02 21:48:53 -0500

    Circle Around Bug Fix
    Bug Fix: Circle around time error when > 1 rotation.

--- QGISPlugin/MilkMachine/resources_rc.py
+++ QGISPlugin/MilkMachine/resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Oct 22 21:30:20 2014
+# Created: Sun Nov 2 20:24:54 2014
 #      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
